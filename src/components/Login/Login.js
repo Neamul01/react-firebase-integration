e0497fa1216393237da7e332f6a1e0ee2aebfae5
Neamul01 <neamulhaque2002@gmail.com>
2022-04-09 17:42:41 +0600

    required route remember location added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import { getAuth } from 'firebase/auth';
 import app from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const auth = getAuth(app)
@@ -9,6 +10,16 @@ const auth = getAuth(app)
 const Login = () => {
     // const { signInWithGoogle } = useFirebase();
     const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+    const location = useLocation();
+    const navigate = useNavigate()
+    const from = location.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
     return (
         <div>
             <h3>Please LogIn!!</h3>
@@ -20,7 +31,7 @@ const Login = () => {
                 <input type="submit" value="LogIn" />
             </form>
             <div style={{ margin: '1rem' }}>
-                <button onClick={() => signInWithGoogle()}>SignIn With Google</button>
+                <button onClick={handleGoogleSignIn}>SignIn With Google</button>
             </div>
         </div>
     );
